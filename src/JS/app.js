a692929fa57e3966db10abd70d4eb5206d127d81
Harry Craven <HarryCraven.Dev@gmail.com>
2020-07-23 18:41:25 +0100

    Added submitPost method and alert

--- src/JS/app.js
+++ src/JS/app.js
@@ -7,9 +7,31 @@ const getPosts = () => {
     .get("http://localhost:3000/posts")
     .then((data) => ui.getPosts(data))
     .catch((err) => console.log(err));
+};
 
-  console.log("working");
+// Submit Posts
+const submitPosts = () => {
+  // Posts Title & Body
+  const title = document.getElementById("title").value;
+  const body = document.getElementById("body").value;
+
+  const data = {
+    title,
+    body,
+  };
+
+  http
+    .post("http://localhost:3000/posts", data)
+    .then((data) => {
+      ui.showAlert("Post added!", "alert alert-success");
+      ui.clearFields();
+      getPosts(data);
+    })
+    .catch((err) => console.log(err));
 };
 
 // Get posts on DOM load
 document.addEventListener("DOMContentLoaded", getPosts);
+
+// Listen for add post
+document.querySelector(".post-submit").addEventListener("click", submitPosts);
--- src/JS/ui.js
+++ src/JS/ui.js
@@ -29,6 +29,41 @@ class UI {
     });
     this.posts.innerHTML = output;
   };
+
+  showAlert = (message, className) => {
+    this.clearAlert();
+
+    // Create div
+    const div = document.createElement("div");
+    // Add class
+    div.className = className;
+    // Add Text
+    div.appendChild(document.createTextNode(message));
+    // Get Parent
+    const container = document.querySelector(".postsContainer");
+    // Get Posts
+    const posts = document.querySelector("#posts");
+    // Insert alert div
+    container.insertBefore(div, posts);
+
+    // Timeout for clearAlert
+    setTimeout(() => {
+      this.clearAlert();
+    }, 3000);
+  };
+
+  clearAlert = () => {
+    const currentAlert = document.querySelector(".alert");
+
+    if (currentAlert) {
+      currentAlert.remove();
+    }
+  };
+
+  clearFields = () => {
+    this.titleInput.value = "";
+    this.bodyInput.value = "";
+  };
 }
 
 export const ui = new UI();
