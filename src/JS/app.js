d715cb18a7e94e9ef0cf0f2c019a1a8bef62822f
Harry Craven <HarryCraven.Dev@gmail.com>
2020-07-23 11:12:57 +0100

    Added ui.js containing a ui class, http.js to act as REST API.

--- src/JS/app.js
+++ src/JS/app.js
@@ -1,1 +1,15 @@
+import { http } from "./http";
+import { ui } from "./ui";
 
+// Get Posts
+const getPosts = () => {
+  http
+    .get("http://localhost:3000/posts")
+    .then((data) => ui.getPosts(data))
+    .catch((err) => console.log(err));
+
+  console.log("working");
+};
+
+// Get posts on DOM load
+document.addEventListener("DOMContentLoaded", getPosts);
